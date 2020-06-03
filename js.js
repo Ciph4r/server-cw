087c8bb20cc68094e44d0e0dcfe1cdf085038c59
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-06-03 14:13:51 -0400

    'done'

--- js.js
+++ js.js
@@ -1,27 +1,27 @@
 const htttp = require('http')
 const fs = require('fs')
  const server = htttp.createServer((req , res)=> {
-    readStream = fs.createReadStream(__dirname + '/error.html' , 'utf8')
+   let readStream = ''
  
 
      if(req.url === '/'){
-        readStream = fs.createReadStream(__dirname + '/error.html' , 'utf8')
-        res.writeHead(500,{ 'Content-Type' : 'text/plain'})
+        readStream = fs.createReadStream(__dirname + '/index.html' , 'utf8')
+        res.writeHead(200,{ 'Content-Type' : 'text/plain'})
         readStream.pipe(res)
         
      }
- 
-    if (req.url === '/about'){
+  
+   else if (req.url === '/about'){
         readStream = fs.createReadStream(__dirname + '/about.html' , 'utf8')
         readStream.pipe(res)
         
     }
-    if (req.url === '/user'){
+    else if (req.url === '/user'){
         res.writeHead(200,{ 'Content-Type' : 'application/json' })
         res.write(JSON.stringify({user: ['dave' , 'denis' , 'ginny']}))
         
     }
-    if (req.url === undefined){
+    else {
         readStream = fs.createReadStream(__dirname + '/error.html' , 'utf8')
         readStream.pipe(res)
     }
